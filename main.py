6db6d143ef714334985ea2d27bf031cf8c5ef3b3
lurplee <eniolafama@gmail.com>
2026-04-20 08:56:27 -0400

    created accounts + transactions table and banking app functions

---
+++ main.py
@@ -0,0 +1,111 @@
+import mysql.connector
+import random
+
+# Connect to your MySQL database
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="gearup4me",
+    database="banking_app"
+)
+cursor = conn.cursor()
+
+# Create the contacts table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS accounts (
+        user_id INT PRIMARY KEY,
+        password varchar(255),
+        first_name varchar(255),
+        last_name varchar(255),
+        balance decimal(65,2)
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS transactions (
+        trans_id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id int,
+        transaction_type varchar(255),
+        amount DECIMAL(65,2),
+        date DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id)
+        REFERENCES accounts(user_id)
+        
+    )
+''')
+
+
+def create_account():
+    print("CREATE AN ACCOUNT")
+    first_name = input("First Name: ")
+    last_name = input("Last Name: ")
+    password = input("Password: ")
+
+    # user_id made global b/c used in every function
+    user_id = random.randrange(100,900)
+    
+
+    print("ACCOUNT INFO")
+    print(f"First Name: {first_name}\nLast Name: {last_name}\nUser ID: {user_id}\nPassword: {password}")
+
+
+    print("ACCOUNT CREATED!")
+    # Updating Accounts Table
+
+    cursor.execute("INSERT INTO accounts VALUES (%s, %s, %s, %s, %s)", (user_id, password, first_name, last_name, 0.00))
+    conn.commit()
+
+    return user_id
+
+
+
+def do_transaction(user_id):
+    
+    while True:
+        try:
+            transaction_type = input("[W] Withdraw\n[D] Deposit").upper()
+            cursor.execute("SELECT balance FROM accounts WHERE user_id = %s", (user_id,))
+            balance = float(cursor.fetchone()[0])
+
+            if transaction_type != "W" and transaction_type != "D":
+                raise ValueError
+            
+            if transaction_type == "W":
+                #Select From accounts, current balance
+                
+                
+
+                amount = float(input(f"Current Balance: ${balance}\nWithdraw Amount: $"))
+                while balance < amount:
+                    print("Invalid Withdraw Amount, Please Try Again!")
+                    amount = float(input("Withdraw Amount: $"))
+                
+                amount = round(amount,2)
+                
+                old_balance = balance
+                balance -= amount
+                print(f"WITHDRAWAL SUCCESS!\nOld Balance: ${old_balance}\nNew Balance: ${balance}")
+
+
+            if transaction_type == "D":
+
+                amount = float(input(f"Current Balance: ${balance}\nDeposit Amount: $"))
+                amount = round(amount,2)
+                old_balance = balance
+                balance += amount
+                print(f"DEPOSIT SUCCESS!\nOld Balance: ${old_balance}\nNew Balance: ${balance}")
+
+            #Update Transaction & Account Tables
+
+            cursor.execute("INSERT INTO transactions(user_id, transaction_type,amount) VALUES(%s,%s,%s)", (user_id,transaction_type,amount))
+            cursor.execute("UPDATE accounts SET balance = %s WHERE user_id = %s", (balance,user_id))
+            conn.commit()
+            break
+        except:
+            print("Invalid Input!")
+
+
+user_id = create_account()
+do_transaction(user_id)
+
+conn.close()
