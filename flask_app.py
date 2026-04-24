0c5c771c48c4fb537b89378ec4682df65524dbde
lurplee <eniolafama@gmail.com>
2026-04-24 14:26:05 -0400

    Added more admin features (delete accounts, transfer to all accounts, create accounts for users) + bug fixes

--- flask_app.py
+++ flask_app.py
@@ -33,12 +33,13 @@ def create_app(test_config=None):
         user_id = int(user_id)
 
         all_accounts = main.get_all_accounts()
-        user_name = main.get_name(user_id)
         accounts = main.get_accounts(user_id)
-        total_balance = main.get_total_balance(accounts)
+        user_name = main.get_name(user_id)
+        total_balance = main.get_total_balance(all_accounts)
+        total_user_balance = main.get_user_balance(user_id)
 
 
-        return render_template("home.html",accounts = accounts, user_name = user_name, user_id = user_id, total_balance = total_balance, all_accounts=all_accounts)
+        return render_template("home.html",accounts = accounts, user_name = user_name, user_id = user_id, total_balance = total_balance, all_accounts=all_accounts, total_user_balance=total_user_balance)
     
     @app.route('/delete_account', methods=["GET", "POST"])
     def delete_account():
@@ -47,7 +48,7 @@ def create_app(test_config=None):
         update= None
 
         if request.method == 'POST':
-            deleted_account = int(request.form["chosen_account"])
+            deleted_account = int(request.form.get(["chosen_account"]))
             deleted_account_name = main.get_account_name(deleted_account)
             delete_success = main.delete_account(deleted_account)    
 
@@ -66,8 +67,8 @@ def create_app(test_config=None):
     @app.route('/login', methods=["GET", "POST"])
     def login():
         if request.method == 'POST':
-            entered_user_id = request.form["entered_user_id"]
-            entered_password = request.form["entered_password"]
+            entered_user_id = request.form.get("entered_user_id")
+            entered_password = request.form.get("entered_password")
 
             password= main.get_password(entered_user_id)
 
@@ -88,9 +89,11 @@ def create_app(test_config=None):
     @app.route('/past_transactions')
     def past_transactions():
         user_id = session.get("user_id")
+        user_id = int(user_id)
         user_name = main.get_name(user_id)
         transactions = main.get_transactions(user_id)
-        return render_template("past_transactions.html",transactions=transactions, user_name = user_name)
+        all_transactions = main.get_all_transactions()
+        return render_template("past_transactions.html",transactions=transactions,all_transactions=all_transactions,user_name = user_name, user_id=user_id)
 
     @app.route('/withdraw', methods=["GET", "POST"])
     def withdraw():
@@ -100,9 +103,9 @@ def create_app(test_config=None):
         account_id =None
 
         if request.method == 'POST':
-            account_id = request.form["account_id"]
+            account_id = request.form.get("account_id")
 
-            amount = float(request.form["amount"])
+            amount = float(request.form.get("amount"))
             amount = round(amount,2)
 
             success = main.withdraw(user_id, account_id, amount)
@@ -120,7 +123,7 @@ def create_app(test_config=None):
 
         if (account_id != None):
             account_id = int(account_id)
-            balance = main.get_balance(user_id, account_id)
+            balance = main.get_balance(account_id)
 
         return render_template("withdraw.html", accounts=accounts, balance=balance, account_id=account_id)
     
@@ -128,18 +131,23 @@ def create_app(test_config=None):
     @app.route('/transfer', methods=["GET", "POST"])
     def transfer():
         user_id = session.get("user_id")
-        accounts = main.get_accounts(user_id)
+        user_id = int(user_id)
+
+        if user_id ==100:
+            accounts = main.get_all_accounts()
+        else:
+            accounts = main.get_accounts(user_id)
         balance = None
         account_id =None
 
         if request.method == 'POST':
-            account_from = request.form["account_1"]
-            account_to = request.form["account_2"]
+            account_from = request.form.get("account_1")
+            account_to = request.form.get("account_2")
 
-            amount = float(request.form["amount"])
+            amount = float(request.form.get("amount"))
             amount = round(amount,2)
 
-            success = main.transfer(user_id, account_from,account_to, amount)
+            success = main.transfer(account_from,account_to, amount)
 
             if success:
                 #Redirects so that the selected account's balance shows up
@@ -154,9 +162,9 @@ def create_app(test_config=None):
 
         if (account_id != None):
             account_id = int(account_id)
-            balance = main.get_balance(user_id, account_id)
+            balance = main.get_balance(account_id)
 
-        return render_template("transfer.html", accounts=accounts, balance=balance, account_id=account_id)
+        return render_template("transfer.html", accounts=accounts,balance=balance, account_id=account_id, user_id=user_id)
     
     @app.route('/deposit', methods=["GET", "POST"])
     def deposit():
@@ -166,9 +174,9 @@ def create_app(test_config=None):
         account_id =None
 
         if request.method == 'POST':
-            account_id = request.form["account_id"]
+            account_id = request.form.get("account_id")
 
-            amount = float(request.form["amount"])
+            amount = float(request.form.get("amount"))
             amount = round(amount,2)
 
             main.deposit(user_id, account_id, amount)
@@ -182,7 +190,7 @@ def create_app(test_config=None):
         if (account_id != None):
             
             int(account_id)
-            balance = main.get_balance(user_id, int(account_id))
+            balance = main.get_balance(int(account_id))
 
   
         return render_template("deposit.html", accounts=accounts, balance=balance, account_id=account_id)
@@ -190,10 +198,10 @@ def create_app(test_config=None):
     @app.route('/register', methods=['GET', 'POST'])
     def register():
         if request.method == 'POST':
-            first_name = request.form["first_name"]
-            last_name = request.form["last_name"]
-            password = request.form["password"]
-            phone_number = request.form["phone_number"]
+            first_name = request.form.get("first_name")
+            last_name = request.form.get("last_name")
+            password = request.form.get("password")
+            phone_number = request.form.get("phone_number")
             user_id = main.create_user(first_name, last_name, password,phone_number)
             session["user_id"] = user_id
             return redirect(url_for("home"))
@@ -201,13 +209,59 @@ def create_app(test_config=None):
 
     @app.route('/create_account', methods=['GET', 'POST'])
     def create_account():
-        if request.method == 'POST':
-            user_id = session.get("user_id")
-            account_name = request.form["account_name"]
-            initial_deposit = float(request.form["initial_deposit"])
-            main.create_account(user_id, account_name,initial_deposit)
+        selected_user_id = None
+        all_users = main.get_all_users()
+        user_id = session.get("user_id")
+        user_id = int(user_id)
 
-        return render_template("create_account.html")
+        if request.method == 'POST':
+
+            account_name = request.form.get("account_name")
+            initial_deposit = float(request.form.get("initial_deposit"))
+            
+
+            if user_id == 100:
+                selected_user_id = int(request.form.get("selected_user_id"))
+                main.create_account(user_id, account_name,initial_deposit, True, selected_user_id)
+            else:
+                main.create_account(user_id, account_name,initial_deposit)
+
+        return render_template("create_account.html", all_users=all_users, user_id=user_id)
+    
+    @app.route('/modify_account', methods=['GET', 'POST'])
+    def modify_account():
+        account_id=None
+        account_name = None
+        balance = None
+        user_id = None
+        all_accounts = main.get_all_accounts()
+
+
+        if request.method == 'POST':
+            account_id = request.form.get("account_id")
+            print(request.form)
+            if account_id:
+                account_id = int(account_id)
+                user_id = main.get_user_id(account_id)
+                new_balance = request.form.get("new_balance")
+                new_account_name = request.form.get("new_account_name")
+                if new_balance:
+                    new_balance = float(new_balance)
+                    main.update_balance(new_balance, account_id)
+                if new_account_name:
+                    main.update_account_name(new_account_name, account_id)
+            return redirect(url_for("modify_account", account_id=account_id,user_id=user_id))
+        
+            
+        
+        # Saves account_id upon reload
+        account_id = request.args.get("account_id")
+
+        if account_id:
+                account_id = int(account_id)
+                account_name = str(main.get_account_name(account_id))
+                balance = main.get_balance(account_id)
+        return render_template("modify_account.html", account_id=account_id, all_accounts=all_accounts, account_name = account_name, balance=balance, user_id=user_id)
 
     return app
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -12,13 +12,13 @@ conn = mysql.connector.connect(
 cursor = conn.cursor()
 
 # UNCOMMENT TO RESET DATABASE
-# cursor.execute("DROP DATABASE banking_app")
-# cursor.execute("CREATE DATABASE banking_app")
-# cursor.execute("USE banking_app")
-# cursor.execute("DROP TABLE IF EXISTS accounts")
-# cursor.execute("DROP TABLE IF EXISTS transactions")
-# cursor.execute("DROP TABLE IF EXISTS users")
-# conn.commit()
+cursor.execute("DROP DATABASE banking_app")
+cursor.execute("CREATE DATABASE banking_app")
+cursor.execute("USE banking_app")
+cursor.execute("DROP TABLE IF EXISTS accounts")
+cursor.execute("DROP TABLE IF EXISTS transactions")
+cursor.execute("DROP TABLE IF EXISTS users")
+conn.commit()
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
@@ -31,7 +31,7 @@ cursor.execute('''
         join_date DATETIME DEFAULT CURRENT_TIMESTAMP
 
         
-    )
+    ) AUTO_INCREMENT =100
 ''')
 
 cursor.execute('''
@@ -48,23 +48,20 @@ cursor.execute('''
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS transactions (
         trans_id INT AUTO_INCREMENT PRIMARY KEY,
-        user_id int,
         account_id int,
         transaction_type varchar(255),
         amount DECIMAL(65,2),
         date DATETIME DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id)
-        REFERENCES users(user_id),
         FOREIGN KEY (account_id)
         REFERENCES accounts(account_id)
         
     )
 ''')
 def check_admin():
-    cursor.execute("SELECT user_id FROM users WHERE user_id = %s", (1,))
+    cursor.execute("SELECT user_id FROM users WHERE user_id = %s", (100,))
     admin_info = cursor.fetchall()
 
-    if admin_info == None:
+    if admin_info !=100:
         cursor.execute("INSERT INTO users  (password, first_name, last_name, total_balance, phone_number) VALUES ( %s, %s, %s, %s,%s)", ("adminpassword", "Admin", "Admin",0.0, 000000))
         conn.commit()
         print("Admin Created!")
@@ -74,11 +71,30 @@ def check_admin():
 
 check_admin()
 
+def get_all_users():
+    cursor.execute("SELECT * FROM users")
+    all_users= cursor.fetchall()
+    return all_users
 
-def get_balance(user_id, account_id):
-    cursor.execute("SELECT balance FROM accounts WHERE user_id = %s AND account_id = %s", (user_id,account_id))
+def get_balance(account_id):
+    cursor.execute("SELECT balance FROM accounts WHERE account_id = %s", (account_id,))
     balance = float(cursor.fetchone()[0])
     return balance
+
+def update_balance(new_balance, account_id):
+    cursor.execute("UPDATE accounts SET balance = %s WHERE account_id = %s", (new_balance, account_id))
+    conn.commit()
+    return True
+
+def update_account_name(new_account_name, account_id):
+    cursor.execute("UPDATE accounts SET account_name = %s WHERE account_id = %s", (new_account_name, account_id))
+    conn.commit()
+    return True
+def get_user_id(account_id):
+    cursor.execute("SELECT user_id FROM accounts WHERE account_id = %s", (account_id,))
+    user_id= cursor.fetchone()[0]
+    user_id = int(user_id)
+    return user_id
 
 def get_name(user_id):
     cursor.execute("SELECT first_name FROM users WHERE user_id = %s", (user_id,))
@@ -104,7 +120,8 @@ def get_phone_number(user_id):
 
 def get_account_name(account_id):
     cursor.execute("SELECT account_name FROM accounts WHERE account_id = %s", (account_id,))
-    account_name = "Yeehaw"
+
+    account_name = cursor.fetchone()[0]
     # account_name = (cursor.fetchone()[0])
     return account_name
 
@@ -112,18 +129,36 @@ def get_account_name(account_id):
 def create_user(first_name,last_name,password, phone_number):
     # Updating Users Table
 
-    cursor.execute("INSERT INTO users (password, first_name, last_name, total_balance, phone_number) VALUES (%s, %s, %s, %s,%s)", (password, first_name, last_name,0.0, phone_number))
+    cursor.execute("INSERT INTO users (password, first_name, last_name, total_balance, phone_number) VALUES (%s, %s, %s, %s,%s)", (password, first_name, last_name,0.00, phone_number))
     conn.commit()
     user_id = cursor.lastrowid
     return user_id
 
-def create_account(user_id, account_name,initial_deposit):
-    cursor.execute("INSERT INTO accounts (user_id,account_name, balance) VALUES (%s, %s, %s)", (user_id, account_name, initial_deposit))
-    conn.commit()
+def get_user_balance(user_id):
+    cursor.execute("SELECT total_balance FROM users WHERE user_id = %s", (user_id,))
+    user_balance = cursor.fetchone()[0]
+    return user_balance
 
-def transfer(user_id,account_from,account_to,  amount):
-    balance_1 = get_balance(user_id, account_from)
-    balance_2 = get_balance(user_id, account_to)
+
+def create_account(user_id, account_name,initial_deposit, admin= False, selected_user_id = None):
+    if admin:
+        current_balance = get_user_balance(selected_user_id)
+        new_balance = float(current_balance) + float(initial_deposit)
+        print(current_balance)
+        cursor.execute("INSERT INTO accounts (user_id,account_name, balance) VALUES (%s, %s, %s)", (selected_user_id, account_name, initial_deposit))
+        cursor.execute("UPDATE users SET total_balance = %s WHERE user_id = %s", (new_balance,selected_user_id))
+        conn.commit()
+    else:
+        current_balance = get_user_balance(user_id)
+        new_balance = float(current_balance) + float(initial_deposit)
+        print(current_balance)
+        cursor.execute("INSERT INTO accounts (user_id,account_name, balance) VALUES (%s, %s, %s)", (user_id, account_name, initial_deposit))
+        cursor.execute("UPDATE users SET total_balance = %s WHERE user_id = %s", (new_balance,user_id))
+        conn.commit()
+
+def transfer(account_from,account_to,  amount):
+    balance_1 = get_balance(account_from)
+    balance_2 = get_balance(account_to)
 
     if balance_1 < amount:
         return False
@@ -131,14 +166,14 @@ def transfer(user_id,account_from,account_to,  amount):
     balance_1 -= amount
     balance_2+=amount
 
-    cursor.execute( "INSERT INTO transactions(user_id, account_id, transaction_type, amount) VALUES(%s,%s,%s,%s)",(user_id, account_from, "Transfer Out", amount)
+    cursor.execute( "INSERT INTO transactions(account_id, transaction_type, amount) VALUES(%s,%s,%s)",(account_from, "Transfer Out", amount)
     )
     cursor.execute(
-        "INSERT INTO transactions(user_id, account_id, transaction_type, amount) VALUES(%s,%s,%s,%s)",(user_id, account_to, "Transfer In", amount)
+        "INSERT INTO transactions(account_id, transaction_type, amount) VALUES(%s,%s,%s)",(account_to, "Transfer In", amount)
     )
 
-    cursor.execute("UPDATE accounts SET balance = %s WHERE user_id = %s AND account_id = %s", (balance_1, user_id, account_from))
-    cursor.execute("UPDATE accounts SET balance = %s WHERE user_id = %s AND account_id = %s",(balance_2, user_id, account_to))
+    cursor.execute("UPDATE accounts SET balance = %s WHERE account_id = %s", (balance_1, account_from))
+    cursor.execute("UPDATE accounts SET balance = %s WHERE account_id = %s",(balance_2, account_to))
 
     return True
 
@@ -156,17 +191,23 @@ def get_total_balance(accounts):
     total_balance =0
     if accounts != None:
         for account in accounts:
-            int(account[2])
-            total_balance +=account[2]
+            int(account[3])
+            total_balance +=account[3]
     return total_balance
 
 def get_accounts(user_id):
-    cursor.execute("SELECT account_id, account_name, balance FROM accounts WHERE user_id =%s", (user_id,))
+    cursor.execute("SELECT * FROM accounts WHERE user_id =%s", (user_id,))
     accounts = cursor.fetchall()
     return accounts
 
 def get_transactions(user_id):
-    cursor.execute("SELECT trans_id, account_id, transaction_type, amount, date FROM transactions WHERE user_id = %s ORDER BY date DESC", (user_id,))
+    cursor.execute("""
+    SELECT t.trans_id, t.account_id, t.transaction_type, t.amount, t.date
+    FROM transactions t
+    JOIN accounts a ON t.account_id = a.account_id
+    WHERE a.user_id = %s
+    ORDER BY t.date DESC
+""", (user_id,))
     transactions = cursor.fetchall()
     result = []
 
@@ -178,6 +219,7 @@ def get_transactions(user_id):
         result.append({
             "transaction_id": trans_id,
             "account_name": account_name,
+            "account_id": account_id,
             "type": transaction_type,
             "amount": float(amount),
             "date": date
@@ -185,8 +227,30 @@ def get_transactions(user_id):
 
     return result
 
+def get_all_transactions():
+    cursor.execute("SELECT trans_id, account_id, transaction_type, amount, date FROM transactions ORDER BY date DESC")
+    transactions = cursor.fetchall()
+    result = []
+
+    for trans_id, account_id, transaction_type, amount, date in transactions:
+        
+        cursor.execute("SELECT account_name FROM accounts WHERE account_id = %s", (account_id,))
+        account_name = cursor.fetchone()[0]
+
+        result.append({
+            "transaction_id": trans_id,
+            "account_name": account_name,
+            "account_id": account_id,
+            "type": transaction_type,
+            "amount": float(amount),
+            "date": date
+        })
+
+    return result
+
+
 def withdraw (user_id, account_id, amount):
-    balance = get_balance(user_id, account_id)
+    balance = get_balance(account_id)
     accounts = get_accounts(user_id)
     if balance < amount:
         return False
@@ -194,7 +258,7 @@ def withdraw (user_id, account_id, amount):
     #Update Transaction & Account Tables
 
     total_balance = get_total_balance(accounts)
-    cursor.execute("INSERT INTO transactions(user_id, account_id, transaction_type,amount) VALUES(%s,%s,%s, %s)", (user_id,account_id,"Withdraw",amount))
+    cursor.execute("INSERT INTO transactions( account_id, transaction_type,amount) VALUES(%s,%s,%s)", (account_id,"Withdraw",amount))
     cursor.execute("UPDATE accounts SET balance = %s WHERE user_id = %s AND account_id = %s", (balance, user_id, account_id))
     cursor.execute("UPDATE users SET total_balance = %s WHERE user_id =%s",(total_balance,user_id) )
 
@@ -207,16 +271,15 @@ def get_all_accounts():
     return all_accounts
 
 
-
 def deposit (user_id, account_id,amount):
-    balance = get_balance(user_id,account_id)
+    balance = get_balance(account_id)
     accounts = get_accounts(user_id)
     balance += amount
 
     #Update Transaction & Account Tables
 
     total_balance = get_total_balance(accounts)
-    cursor.execute("INSERT INTO transactions(user_id, account_id, transaction_type,amount) VALUES(%s,%s,%s, %s)", (user_id,account_id,"Deposit",amount))
+    cursor.execute("INSERT INTO transactions(account_id, transaction_type,amount) VALUES(%s,%s, %s)", (account_id,"Deposit",amount))
     cursor.execute("UPDATE accounts SET balance = %s WHERE user_id = %s AND account_id = %s", (balance, user_id, account_id))
     cursor.execute("UPDATE users SET total_balance = %s WHERE user_id =%s",(total_balance,user_id) )
     conn.commit()
